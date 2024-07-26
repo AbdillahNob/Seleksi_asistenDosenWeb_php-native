943b9281cc1804c62b117466298c32330117a137
kwangmin123455 <abdillah.kuhaku@gmail.com>
2024-07-27 02:33:50 +0800

    Complete Fitur Register dan Login

---
+++ function/function.php
@@ -0,0 +1,51 @@
+<?php 
+ $conn = mysqli_connect("localhost","root","","db_asistendosen");
+
+
+function view($query){
+        global $conn;
+
+        $result = mysqli_query($conn, $query);
+         
+        return $result;
+    }
+
+function insert($data, $no_file){
+    global $conn;
+
+    // no_file
+    // 1. user
+
+    if($no_file == 1){
+        $username = strtolower(stripslashes($data['username']));
+        $nomor = mysqli_real_escape_string($conn, $data['nomor']);
+        $password = mysqli_real_escape_string($conn, $data['password']);
+        $status = $data['status'];
+        
+        if($status == 'admin'){
+
+            $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE status='$status'");
+
+            // Validasi agar admin hanya 1 akun
+            if(mysqli_num_rows($result) > 0){
+                echo "
+                    <script>
+                        alert('Maaf Admin hanya boleh 1 akun !');
+                    </script>
+                ";
+                return false;
+            }
+        }
+         
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO tb_user (username, nomor, password, status) VALUES ('$username','$nomor','$password','$status')";
+
+    }else{
+        return false;
+    }
+
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
+
+?>
--- register.php
+++ register.php
@@ -1,47 +1,49 @@
 <?php 
+
 require 'function/function.php';
 
-if(isset($_POST['login']) && isset($_POST['status']) ){
 
-    $username = $_POST['username'];    
-    $result = view("SELECT * FROM tb_user WHERE username='$username'");
+if(isset($_POST['submit']) && isset($_POST['status'])){
 
-    // 1. Validasi Username
-    if(mysqli_num_rows($result) > 0){
-        $status = $_POST['status'];        
-        $row = mysqli_fetch_assoc($result);
+    $no_file = $_GET['no_file'];
+    $password = $_POST['password'];
+    $rePassword = $_POST['rePassword'];
+    $nomor = $_POST['nomor'];
 
-        // 2. Validasi status
-        if($status == $row['status']){  
-            $password = $_POST['password'];
+    // 1. Validasi Konfirmasi Password
+    if($password === $rePassword ){
 
-            // 3. Validasi Password
-            if(password_verify($password, $row['password'])){
+        $resultN = view("SELECT * FROM tb_user WHERE nomor='$nomor'");
+        // 2. Validasi agr Nim/Nid hanya blh 1 atau nim yg di input sdh di daftar sebelumnya.
+        if(mysqli_num_rows($resultN) == 0){
+                        
+            // 3. Validasi Insert dan perintah masukkan Data
+            if(insert($_POST, $no_file) > 0){
                 echo"
                 <script type='text/javascript'>
                     setTimeout(function () {
                         Swal.fire({
                             title: 'INFO',
-                            text: 'Berhasil Login',
+                            text: 'Berhasil Register Akun',
                             icon: 'success',
                             timer: '3200',
                             showConfirmButton: false
                         });
                     },10);
                     window.setTimeout(function(){
-                        window.location.replace('dashboard.php');
+                        window.location.replace('index.php');
                     },2500);
                 </script>
                 ";
 
-            // 3. End Validasi Password
+            // 3. End Validasi dan perintah Insert 
             }else{
                 echo"
                 <script type='text/javascript'>
                     setTimeout(function () {
                         Swal.fire({
                             title: 'INFO',
-                            text: 'Password Anda Salah!',
+                            text: 'Gagal Register Akun',
                             icon: 'warning',
                             timer: '3200',
                             showConfirmButton: false
@@ -53,49 +55,47 @@ if(isset($_POST['login']) && isset($_POST['status']) ){
                 </script>
                 ";
             }
-                  
-        //2. End Validasi Status
+            
+        // 2. End Validasi Nim/Nid
         }else{
             echo"
+                <script type='text/javascript'>
+                    setTimeout(function () {
+                        Swal.fire({
+                            title: 'INFO',
+                            text: 'Nim/Nid anda sudah terdaftar sebelumnya',
+                            icon: 'warning',
+                            timer: '3200',
+                            showConfirmButton: false
+                        });
+                    },10);
+                    window.setTimeout(function(){
+                        window.location.replace('register.php');
+                    },2500);
+                </script>
+                ";
+        }
+
+    // 1. End Validasi Konfirmasi Password  
+    }else{
+        echo"
             <script type='text/javascript'>
                 setTimeout(function () {
                     Swal.fire({
                         title: 'INFO',
-                        text: 'Status Anda Salah!',
+                        text: 'Konfirmasi Password anda salah!',
                         icon: 'warning',
                         timer: '3200',
                         showConfirmButton: false
                     });
                 },10);
                 window.setTimeout(function(){
-                    window.location.replace('index.php');
+                    window.location.replace('register.php');
                 },2500);
             </script>
-            ";
-        }
-                
-    //1. End Validasi Username 
-    }else{
-              
-        echo"
-        <script type='text/javascript'>
-            setTimeout(function () {
-                Swal.fire({
-                    title: 'INFO',
-                    text: 'Maaf username Anda belum terdaftar !',
-                    icon: 'warning',
-                    timer: '3200',
-                    showConfirmButton: false
-                });
-            },10);
-            window.setTimeout(function(){
-                window.location.replace('index.php');
-            },2500);
-        </script>
         ";
     }
 }
-
 
 ?>
 
@@ -105,14 +105,13 @@ if(isset($_POST['login']) && isset($_POST['status']) ){
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta na me="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Quixlab - Bootstrap Admin Dashboard Template by Themefisher.com</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
-    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
-    <script src="sweetalert2.min.js"></script>
-    <link rel="stylesheet" href="sweetalert2.min.css">
 
     <!-- SweetAlert2 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
@@ -120,33 +119,20 @@ if(isset($_POST['login']) && isset($_POST['status']) ){
     <!-- SweetAlert2 JS -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
 
+
     <style>
-    /* CSS Kustom untuk select */
-    .custom-select {
-        background-color: #007bff;
-        /* Warna latar belakang */
-        color: white;
-        /* Warna teks */
-        border: 1px solid #007bff;
-        /* Warna border */
-    }
-
-    .custom-select option {
-        background-color: #007bff;
-        /* Warna latar belakang untuk opsi */
-        color: white;
-        /* Warna teks untuk opsi */
-    }
-
-    .custom-select option:hover {
-        background-color: #0056b3;
-        /* Warna latar belakang saat hover */
+    .small-text {
+        color: red;
     }
     </style>
 
 </head>
 
 <body class="h-100">
+
+    <!--*******************
+        Preloader start
+    ********************-->
     <div id="preloader">
         <div class="loader">
             <svg class="circular" viewBox="25 25 50 50">
@@ -154,6 +140,13 @@ if(isset($_POST['login']) && isset($_POST['status']) ){
             </svg>
         </div>
     </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+
+
+
+
 
     <div class="login-form-bg h-100">
         <div class="container h-100">
@@ -162,40 +155,52 @@ if(isset($_POST['login']) && isset($_POST['status']) ){
                     <div class="form-input-content">
                         <div class="card login-form mb-0">
                             <div class="card-body pt-5">
+
                                 <a class="text-center" href="index.html">
-                                    <h4>Seleksi Asisten Dosen</h4>
+                                    <h4>Register Akun Anda</h4>
                                 </a>
 
-                                <form class="mt-5 mb-5 login-input" method="post" action="">
+                                <form class="mt-5 mb-5 login-input" action="?no_file=1" method="POST">
                                     <div class="form-group">
-                                        <input type="text" class="form-control" name="username"
-                                            placeholder="Masukkan Username Anda">
+                                        <input type="text" class="form-control" placeholder="Buat Username Anda"
+                                            name="username" required>
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" name="password"
-                                            placeholder="Masukkan Password Anda">
+                                        <input type="text" class="form-control" placeholder="Buat Nim/Nid Anda"
+                                            name="nomor" required>
+                                        <small class="small-text">Nim untuk Mahasiswa dan Nid utk Dosen</small>
                                     </div>
+
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" placeholder="Buat Password Anda"
+                                            name="password" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input type="password" class="form-control"
+                                            placeholder="Konfirmasi Password Anda" name="rePassword" required>
+                                    </div>
+
                                     <div class="form-group">
                                         <select class="custom-select" name="status">
                                             <option selected disabled>Status</option>
                                             <option value="mahasiswa">Mahasiswa</option>
                                             <option value="dosen">Dosen</option>
                                             <option value="admin">Admin</option>
-
                                         </select>
                                     </div>
-
-                                    <button class="btn login-form__btn submit w-100" type="submit"
-                                        name="login">Login</button>
+                                    <button class="btn login-form__btn submit w-100" name="submit">Sign in</button>
                                 </form>
-                                <p class="mt-5 login-form__footer">Anda sudah punya Akun? <a href="register.php"
-                                        class="text-primary">Register Akun</a> sekarang</p>
+                                <p class="mt-5 login-form__footer">Have account <a href="page-login.html"
+                                        class="text-primary">Sign Up </a> now</p>
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 
 
@@ -209,7 +214,6 @@ if(isset($_POST['login']) && isset($_POST['status']) ){
     <script src="js/settings.js"></script>
     <script src="js/gleek.js"></script>
     <script src="js/styleSwitcher.js"></script>
-
 </body>
 
 </html>
